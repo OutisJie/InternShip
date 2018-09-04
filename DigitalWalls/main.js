67f096cbfdb78d64b45cdf15c799c47433fa9a85
OutisJie <1014547032@qq.com>
2018-09-04 23:24:16 +0800

    finish script

--- DigitalWalls/main.js
+++ DigitalWalls/main.js
@@ -10,4 +10,44 @@ const scrollDirElements = document.querySelectorAll(SELECTORS.scrollDir);
 
 let currentSectionIndex = 0;
 
-console.log(sectionsArray)
+console.log(sectionsArray ,scrollToElements, scrollDirElements)
+
+const getScrollTarget = dir =>{
+    if (dir === 'prev' && currentSectionIndex > 0){
+        currentSectionIndex --;
+        return sectionsArray[currentSectionIndex];
+    }else if(dir === 'next' && currentSectionIndex < sectionsArray.length - 1){
+        currentSectionIndex ++;
+        return sectionsArray[currentSectionIndex];
+    }
+    return false;
+}
+
+scrollDirElements.forEach(el => {
+    el.addEventListener('click', ()=>{
+        const direction = el.dataset.scrollDir;
+        const target = getScrollTarget(direction);
+
+        if(target){
+            target.scrollIntoView({behavior:'smooth'});
+        }
+    })
+});
+
+scrollToElements.forEach(el => {
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        const targetId = el.getAttribute("href");
+        const target = document.querySelector(targetId);
+
+        if(target) {
+            sectionsArray.forEach((section, index)=>{
+                if(section.id === targetId.replace("#", "")){
+                    currentSectionIndex = index;
+                }
+            })
+
+            target.scrollIntoView({behavior: "smooth"});
+        }
+    })
+})

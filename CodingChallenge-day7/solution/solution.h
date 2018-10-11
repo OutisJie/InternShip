0ea00f8cb0a7d3b1b55fe8d3ee8c7e71a6a40b6b
OutisJie <1014547032@qq.com>
2018-10-11 23:17:09 +0800

    Coding Challenge day7

--- CodingChallenge-day7/solution/solution.h
+++ CodingChallenge-day7/solution/solution.h
@@ -13,6 +13,7 @@ public:
 private:
 	void trimLeft(string& input);
 	void trimRight(string& input);
+	double getMedianIndex(int size1, int size2, vector<int>& nums1, vector<int>& nums2);
 };
 
 Solution::Solution()
@@ -37,6 +38,7 @@ vector<int> Solution::stringToVector(string input) {
 	vector<int> output;
 	trimLeft(input);
 	trimRight(input);
+	input = input.substr(1, input.length() - 2);
 	stringstream ss;
 	string item;
 	ss.str(input);
@@ -49,5 +51,53 @@ vector<int> Solution::stringToVector(string input) {
 }
 
 double Solution::getMedian(vector<int> &nums1, vector<int> &nums2) {
+	int m = nums1.size(), n = nums2.size();
+	double result;
+	if (m > n) {
+		result = getMedianIndex(n, m, nums2, nums1);
+	}
+	else
+	{
+		result = getMedianIndex(m, n, nums1, nums2);
+	}
+	return result;
+}
 
+double Solution::getMedianIndex(int size1, int size2, vector<int> &nums1, vector<int> &nums2) {
+	int iMin = 0, iMax = size1, halfLen = (size1 + size2 + 1) / 2;
+	while (iMin <= iMax) {
+		int i = (iMin + iMax) / 2, j = halfLen - i;
+		if (i < iMax && nums2[j - 1] > nums1[i]) {
+			iMin = i + 1;
+		}
+		else if (i > iMin && nums1[i - 1] > nums2[j]) {
+			iMax = i - 1;
+		}
+		else {
+			int maxLeft = 0;
+			if (i == 0) {
+				maxLeft = nums2[j - 1];
+			}
+			else if (j == 0) {
+				maxLeft = nums1[i - 1];
+			}
+			else {
+				maxLeft = max(nums1[i - 1], nums2[j - 1]);
+			}
+			if ((size1 + size2) % 2 == 1) {
+				return maxLeft;
+			}
+			int minRight = 0;
+			if (i == size1) {
+				minRight = nums2[j];
+			}
+			else if (j == size2) {
+				minRight = nums1[i];
+			}
+			else {
+				minRight = min(nums1[i], nums2[j]);
+			}
+			return (maxLeft + minRight) / 2.0;
+		}
+	}
 }
